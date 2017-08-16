7f55ba398d44448e14caa5ccca176b47a7da552f
Renan Rogério Boni <renan.boni@vbs.corp>
2017-08-16 16:55:14 -0300

    ok

--- app/src/main/java/br/com/carregai/carregai2/service/UpdatingService.java
+++ app/src/main/java/br/com/carregai/carregai2/service/UpdatingService.java
@@ -2,6 +2,8 @@ package br.com.carregai.carregai2.service;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
@@ -21,8 +23,9 @@ public class UpdatingService extends IntentService {
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-        Log.i("Info", "oi");
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-        NotificationUtils.create(getApplicationContext(), 10, intent, R.mipmap.ic_launcher, "TETE", "TESTE");
+        float saldo = sharedPref.getFloat("saldo_atual", 0);
+        NotificationUtils.create(getApplicationContext(), 10, intent, R.mipmap.ic_launcher, "Atenção!", "Seu saldo é de apenas R$" +String.format("%.2f", saldo));
     }
 }
